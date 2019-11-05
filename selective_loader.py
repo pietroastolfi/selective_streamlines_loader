95594fa1f99e69647283acb2c093d3d305769d13
Emanuele Olivetti <emanuele.olivetti@gmail.com>
2019-11-05 16:47:54 +0100

    removed old main code, just used for testing

--- selective_loader.py
+++ selective_loader.py
@@ -149,77 +149,8 @@ if __name__ == '__main__':
 
     apply_affine = True
 
-    print(trk_fn)
-    print("Reading header with nibabel")
-    lazy_trk = nib.streamlines.load(trk_fn, lazy_load=True)
-    header = lazy_trk.header
-    header_size = header['hdr_size']
-    nb_streamlines = header['nb_streamlines']
-    n_scalars = header['nb_scalars_per_point']
-    n_properties = header['nb_properties_per_streamline']
-
-    ## See: http://www.trackvis.org/docs/?subsect=fileformat
-    length_bytes = 4
-    point_size = 3 + n_scalars
-    point_bytes = 4 * point_size
-    properties_bytes = n_properties * 4
-
-    print("Parsing lenghts of %s streamlines" % nb_streamlines)
-    t0 = time()
-    lengths = np.empty(nb_streamlines, dtype=np.int)
-    # In order to reduce the 20x increase in time when reading small
-    # amounts of bytes with NumPy and Python >3.2, we use two
-    # different implementations of the function that parses 4 bytes
-    # into an int32:
-    if float(sys.version[:3]) > 3.2:
-        get_length = get_length_from_bytes
-    else:
-        get_length = get_length_numpy
-        # get_length = get_length_struct
-
-    with open(trk_fn, 'rb') as f:
-        f.seek(header_size)
-        for idx in range(nb_streamlines):
-            l = get_length(f)
-            lengths[idx] = l
-            jump = point_bytes * l + properties_bytes
-            f.seek(jump, 1)
-
-    print("%s sec." % (time() - t0))
-
-    # position in bytes where to find a given streamline in the TRK file:
-    index_bytes = lengths * point_bytes + properties_bytes + length_bytes
-    index_bytes = np.concatenate([[length_bytes], index_bytes[:-1]]).cumsum() + header_size
-    # n_floats = lengths * point_size + n_properties
-    n_floats = lengths * point_size  # better because it skips properties, if they exist
-
-    print("Extracting %s streamlines with the desired id" % len(idxs))
-    t0 = time()
-    streamlines = []
-    with open(trk_fn, 'rb') as f:
-        for idx in idxs:
-            # move to the position initial position of the coordinates
-            # of the streamline:
-            f.seek(index_bytes[idx])
-            # Parse the floats:
-            s = np.fromfile(f, np.float32, n_floats[idx])
-            s.resize(lengths[idx], point_size)
-            # remove scalars if present:
-            if n_scalars > 0:
-                s = s[:, :3]
-
-            streamlines.append(s)
-
-        print("%s sec." % (time() - t0))
-
-    if apply_affine:
-        aff = nib.streamlines.trk.get_affine_trackvis_to_rasmm(lazy_trk.header)
-        streamlines = [nib.affines.apply_affine(aff, s) for s in streamlines]
-
     streamlines2, lengths2 = load_selected_streamlines(trk_fn, idxs,
                                                        apply_affine=apply_affine,
                                                        verbose=True)
 
-    assert((lengths[idxs] == lengths2).all())
-    assert(np.all([(streamlines[i]==streamlines2[i]).all() for i in range(len(streamlines))]))
 
